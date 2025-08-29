b167b7f7d583bbbc58094c558b92975645b926b8
OneFeiFan <3022504358@qq.com>
2025-08-29 15:09:26 +0800

    style(ui): 移除无用的监听器并更新边框颜色

--- components/material-uni/ripple/component.vue
+++ components/material-uni/ripple/component.vue
@@ -30,14 +30,6 @@ import {DEFAULT_CONFIG, DEFAULT_RIPPLE_PROPS} from './config'
 export default {
   name: 'touch-ripple',
   props: {...DEFAULT_RIPPLE_PROPS},
-  watch:{
-    color: function(newVal, oldVal) {
-      console.log("color change", newVal, oldVal)
-    },
-    backgroundColor: function(newVal, oldVal) {
-      console.log("backgroundColor change", newVal, oldVal)
-    }
-  },
   data() {
     return {
       element: null,
