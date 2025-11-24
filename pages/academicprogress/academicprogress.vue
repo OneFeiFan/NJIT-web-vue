143e9d75cdc72c8699648371ad31cfeb44196cf4
OneFeiFan <3022504358@qq.com>
2025-11-24 22:04:18 +0800

    优化UI风格并实现功能

--- pages/academicprogress/academicprogress.vue
+++ pages/academicprogress/academicprogress.vue
@@ -1,32 +1,31 @@
 <template>
   <view :style="[theme,SXData]">
-    <material-nav-bar>
+    <material-nav-bar background-color="var(--md-sys-color-primary)">
       <view class="nav-bar">
-        <uni-icons type="left" size="" @click="back" color="var(--md-sys-color-on-secondary-container)" class="icon-left"/>
+        <uni-icons class="icon-left" color="var(--md-sys-color-on-primary)" size="" type="left" @click="back"/>
         <text class="title">学业进度</text>
-        <uni-icons type="loop" size="" @click="update(true)" color="var(--md-sys-color-on-secondary-container)" class="icon-right"/>
+        <uni-icons class="icon-right" color="var(--md-sys-color-on-primary)" size="" type="loop" @click="update(true)"/>
       </view>
     </material-nav-bar>
+
     <scroll-view scroll-y="true" class="scroll-table">
       <view class="content">
-      <material-card width="100%" :height="mx(28)" color="var(--md-sys-color-on-primary)"
-                     :backgroundColor="'var(--md-sys-color-primary-container)'"
-                     @click="">
-        <view class="card-content">
-          <view class="name">
-            注意
+        <material-card :height="mx(28)"
+                       backgroundColor="var(--md-sys-color-primary-container)"
+                       @click="">
+          <view class="card-content">
+            <view class="name">
+              注意
+            </view>
+            <view class="score">
+              在官方页面，学业进度数据便就是仅供参考的，有些课程不存在或者不用学习，所以某些大类进度不会到100%
+            </view>
           </view>
-          <view class="score">
-            在官方页面，学业进度数据便就是仅供参考的，有些课程不存在或者不用学习，所以某些大类进度不会到100%
-          </view>
-        </view>
-      </material-card>
+        </material-card>
       </view>
       <slot v-for="(item, index) in datas">
-
         <view class="content">
-          <material-card width="100%" color="var(--md-sys-color-on-primary)"
-                         :backgroundColor="'var(--md-sys-color-primary-container)'"
+          <material-card backgroundColor="var(--md-sys-color-primary-container)"
                          @click="">
             <view class="card-content">
               <view class="name">
@@ -40,7 +39,6 @@
                                    buffer-color="var(--md-sys-color-surface-container)"
                                    color="var(--md-sys-color-on-primary-container)"/>
               </view>
-
             </view>
           </material-card>
         </view>
@@ -59,6 +57,7 @@ import MaterialNavBar from "@/components/material-uni/material-nav-bar/material-
 import {getTheme} from "@/components/material-uni/colors";
 import {mx, SXData} from "@/components/material-uni/sx";
 import MaterialProgress from "@/components/material-uni/material-progress/material-progress.vue";
+import {http} from "@/static/util/request";
 
 export default {
   computed: {
@@ -69,7 +68,7 @@ export default {
   components: {MaterialProgress, MaterialNavBar, MaterialCard, UniIcons, UPicker},
   data() {
     return {
-      theme:{},
+      theme: {},
       datas: {},
     }
   },
@@ -81,48 +80,45 @@ export default {
   },
   onShow() {
     this.theme = getTheme()
-    // #ifdef H5
-    this.datas = {
-      "独立设课实验": {"total": 2, "name": "独立设课实验", "completed": 2},
-      "跨专业选修课程": {"total": 2, "name": "跨专业选修课程", "completed": 2},
-      "素质教育课程": {"total": 5, "name": "素质教育课程", "completed": 4},
-      "专业选修课程": {"total": 12, "name": "专业选修课程", "completed": 3},
-      "通识与公共基础课程": {"total": 24, "name": "通识与公共基础课程", "completed": 19},
-      "公共基础实践": {"total": 2, "name": "公共基础实践", "completed": 1},
-      "专业基础课程": {"total": 9, "name": "专业基础课程", "completed": 9},
-      "课程设计": {"total": 4, "name": "课程设计", "completed": 4},
-      "大学外语类课程": {"total": 6, "name": "大学外语类课程", "completed": 6},
-      "毕业设计": {"total": 1, "name": "毕业设计", "completed": 0},
-      "项目训练": {"total": 3, "name": "项目训练", "completed": 3},
-      "实习教学": {"total": 2, "name": "实习教学", "completed": 1},
-      "劳动教育课程": {"total": 2, "name": "劳动教育课程", "completed": 2},
-      "专业课": {"total": 6, "name": "专业课", "completed": 6},
-      "工程技术基础课程": {"total": 2, "name": "工程技术基础课程", "completed": 2},
-      "公共选修课": {"total": 1, "name": "公共选修课", "completed": 1}
-    }
-    // #endif
   },
   methods: {
     mx,
-    refreshTheme(){
+    refreshTheme() {
       this.theme = getTheme()
     },
     back() {
       uni.navigateBack();
     },
-    update(refresh) {
-      uni.showToast({
-        title: '尝试刷新',
-        icon: 'loading',
-        duration: 1000
-      });
+    update(forceRefresh) {
+      if (forceRefresh) {
+        uni.showLoading({
+          title: '尝试刷新'
+        });
+      }
 
-      this.$manager.getAcademicProgress(refresh).then(res => {
-        let value = JSON.parse(res)
-        console.log(value)
-        this.datas = value
+      http.post("/getAcademicProgress", {forceRefresh}).then(res => {
+        if (forceRefresh) {
+          setTimeout(() => {
+            uni.showToast({
+              title: '成功',
+              duration: 2000
+            });
+          }, 500)
+        }
+        this.datas = res.data;
       }).catch(res => {
         console.log(res)
+        if (forceRefresh) {
+          setTimeout(() => {
+            uni.showToast({
+              title: '失败',
+              icon: "error",
+              duration: 2000
+            });
+          }, 500)
+        }
+      }).finally(() => {
+        uni.hideLoading()
       })
     },
   }
@@ -131,7 +127,7 @@ export default {
 
 <style lang="scss">
 .scroll-table {
-  height: calc(100vh - sx(10) - var(--status-bar-height));
+  height: calc(100vh - sx(15) - var(--status-bar-height));
 }
 
 .content {
@@ -159,11 +155,6 @@ export default {
     .name {
       font-size: sx(4.5);
       font-weight: bold;
-      //display: inline-block;
-      //white-space: nowrap;
-      //width: 100%;
-      //overflow: hidden;
-      //text-overflow: ellipsis;
     }
 
     .score {
