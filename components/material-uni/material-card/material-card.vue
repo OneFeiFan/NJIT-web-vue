7f8339f762cd576218ecad017cbb0809316f118e
OneFeiFan <3022504358@qq.com>
2025-11-30 21:30:43 +0800

    采用更原始的方案实现阴影（兼容旧版本浏览器

--- components/material-uni/material-card/material-card.vue
+++ components/material-uni/material-card/material-card.vue
@@ -1,14 +1,19 @@
 <template>
-  <touch-ripple
-      class="card"
-      :color="color"
-      :opacity="opacity"
-      :duration="duration"
-      :transition="transition"
-      :backgroundColor="backgroundColor"
-  >
-    <slot></slot>
-  </touch-ripple>
+  <view class="card-wrapper">
+    <view class="physical-shadow shadow-1"/>
+    <view class="physical-shadow shadow-2"/>
+    <view class="physical-shadow shadow-3"/>
+    <touch-ripple
+        class="card-content"
+        :color="color"
+        :opacity="opacity"
+        :duration="duration"
+        :transition="transition"
+        :backgroundColor="backgroundColor"
+    >
+      <slot></slot>
+    </touch-ripple>
+  </view>
 </template>
 
 <script>
@@ -26,23 +31,45 @@ export default {
       default: "100%"
     },
     ...DEFAULT_RIPPLE_PROPS
-  },
-  methods: {
-    // handleClick(e) {
-    //   this.$emit('click', e);
-    // }
   }
 }
-
 </script>
 
 <style scoped lang="scss">
-.card {
-  width: 100%;
-  //background-color: #ffffff00;
+.card-wrapper {
+  position: relative;
+}
+
+.physical-shadow {
+  position: absolute;
+  inset: 0; // 替代 width/height: 100%
   border-radius: sx(2);
-  box-shadow: 0 3px 5px -1px color-mix(in srgb, var(--md-sys-color-outline), transparent 80%),
-  0 6px 10px 0 color-mix(in srgb, var(--md-sys-color-outline), transparent 86%),
-  0 1px 18px 0 color-mix(in srgb, var(--md-sys-color-outline), transparent 88%);
+  background: var(--md-sys-color-outline);
+  pointer-events: none;
+  z-index: 0;
+}
+
+.shadow-1 {
+  opacity: 0.25;
+  filter: blur(sx(0.75));
+  transform: translate(sx(0.25), sx(0.75));
+}
+
+.shadow-2 {
+  opacity: 0.14;
+  filter: blur(sx(2));
+  transform: translate(sx(0.5), sx(1.7));
+}
+
+.shadow-3 {
+  opacity: 0.12;
+  filter: blur(sx(3.5));
+  transform: translate(sx(0.5), sx(1.7));
+}
+
+.card-content {
+  width: 100%;
+  border-radius: sx(2);
+  z-index: 1; // 放在上层
 }
 </style>
