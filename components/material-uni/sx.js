e5e30e0498d5fb1d87a78805b1358402aa191645
OneFeiFan <3022504358@qq.com>
2026-03-07 18:58:04 +0800

    加入缓存

--- components/material-uni/sx.js
+++ components/material-uni/sx.js
@@ -1,7 +1,15 @@
+let cachedRatio = null;
+
 function getBaseRatio() {
+    if (cachedRatio !== null) {
+        return cachedRatio;
+    }
+
     const sys = uni.getSystemInfoSync();
     const base = (0.4 * sys.windowWidth) + (0.6 * sys.windowHeight);
-    return base / 190;
+    cachedRatio = base / 190;
+
+    return cachedRatio;
 }
 
 export function mx(value) {
